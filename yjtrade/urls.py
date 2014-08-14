99d30970d4ad700cc55786f4ea8d4bb2ef95125a
Pak Youngrok <pak.youngrok@gmail.com>
2014-08-14 17:42:29 +0900

    cyboplus initialize

--- yjtrade/urls.py
+++ yjtrade/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from trade import views
+from trade import views, cyboplus
 
 admin.autodiscover()
 
@@ -13,3 +13,5 @@ urlpatterns = patterns('',
     url(r'^initial_box', views.initial_box),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+cyboplus.initialize()
